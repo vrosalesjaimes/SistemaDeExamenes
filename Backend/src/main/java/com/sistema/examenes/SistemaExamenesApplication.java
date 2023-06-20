c591d745b3ca15a2b1ecc6fa609b715c10ba772c
vrosalesjaimes <vrosalesjaimes@ciencias.unam.mx>
2023-06-19 21:58:48 -0600

    Aplicacion inicial de Spring

---
+++ Backend/src/main/java/com/sistema/examenes/SistemaExamenesApplication.java
@@ -0,0 +1,13 @@
+package com.sistema.examenes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaExamenesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaExamenesApplication.class, args);
+	}
+
+}
