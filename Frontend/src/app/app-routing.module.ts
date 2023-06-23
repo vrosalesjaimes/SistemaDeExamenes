81728db497b31b3a5c57a2f3044aa07bb5911015
vrosalesjaimes <vrosalesjaimes@ciencias.unam.mx>
2023-06-23 16:41:57 -0600

    Creación del panel de detalles de usuario, se han agregado más botones al sidebar y se han hecho modificaciones de estilo en el navbar

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -8,16 +8,21 @@ import { UserDashboardComponent } from './pages/user/user-dashboard/user-dashboa
 import { AdminGuard } from './services/admin.guard';
 import { NormalGuard } from './services/normal.guard';
 import { ProfileComponent } from './pages/profile/profile.component';
+import { WelcomeComponent } from './pages/admin/welcome/welcome.component';
 
 const routes: Routes = [
-  {path:'', component:HomeComponent, pathMatch:'full'},
-  {path:'signup', component:SignupComponent, pathMatch:'full'},
-  {path:'login', component:LoginComponent, pathMatch:'full'},
-  {path: 'admin', component: DashboardComponent, pathMatch:'full', canActivate:[AdminGuard], 
-  children:[
-    {path:'profile', component:ProfileComponent}
-  ]},
-  {path: 'user', component: UserDashboardComponent, pathMatch:'full', canActivate:[NormalGuard]},
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'signup', component: SignupComponent, pathMatch: 'full' },
+  { path: 'login', component: LoginComponent, pathMatch: 'full' },
+  {
+    path: 'admin', component: DashboardComponent, canActivate: [AdminGuard],
+    children: [
+      { path: '', component: WelcomeComponent },
+      { path: 'profile', component: ProfileComponent }, 
+    ]
+  },
+  { path: 'user', component: UserDashboardComponent, pathMatch: 'full', canActivate: [NormalGuard] },
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
--- Frontend/src/app/pages/profile/profile.component.ts
+++ Frontend/src/app/pages/profile/profile.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  user:any = null;
 
+  constructor(private loginService:LoginService){}
+  ngOnInit(): void {
+    this.user = this.loginService.getUser();
+  }
 }
